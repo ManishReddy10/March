2924e841b735d0efe816affb8e664a13facc7101
Manish Reddy <ysmanish5@gmail.com>
2025-05-27 15:23:54 -0700

    Add YOLO model and image for object detection -- basically code from yolo website + print statements

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-# Something
-import os
-import cv2 as cv
+from ultralytics import YOLO
 
+# Load a model
+model = YOLO("yolo11n-pose.pt")  # load an official model
+
+# Predict with the model
+results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+
+# Access the results
+for result in results:
+    xy = result.keypoints.xy  # x and y coordinates
+    print(xy)
+    xyn = result.keypoints.xyn  # normalized
+    print(xyn)
+    kpts = result.keypoints.data  # x, y, visibility (if available)
+    print(kpts)
