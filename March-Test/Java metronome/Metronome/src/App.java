9196a4afcd76741cf50a1381395171507712d3ee
Manish Reddy <ysmanish5@gmail.com>
2025-06-12 20:03:11 -0700

    java metronome functional, however, is not benchmarked yet

--- March-Test/Java metronome/Metronome/src/App.java
+++ March-Test/Java metronome/Metronome/src/App.java
@@ -1,6 +1,8 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
         Metronome myMetronome = new Metronome();
-        myMetronome.playSound("strong_beat.wav");
+        myMetronome.startMetronome(60);
+
     }
 }
--- March-Test/Java metronome/Metronome/src/Metronome.java
+++ March-Test/Java metronome/Metronome/src/Metronome.java
@@ -1,11 +1,14 @@
 import java.util.Timer;
 
+import java.time.Instant;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 public class Metronome {
     Timer timer = new Timer();
     
+    boolean isMetronomeOn = false;
     
     public synchronized void playSound(final String url) {
     new Thread(new Runnable() {
@@ -26,14 +29,30 @@ public class Metronome {
     }).start();
     }
 
-    public void playMetronomeBeat() {
-
+    private void playMetronomeStrongBeat() {
+        playSound("strong_beat.wav");
     }
 
-    public void startMetronome() {
-        // timer
-    }   
-    
+    public void startMetronome(double bpm) {
+
+        
+        isMetronomeOn = true;
+        
+        
+        while (isMetronomeOn) {
+            playMetronomeStrongBeat();
+            synchronized (this) {
+                try {
+                    wait((1/((long) (bpm / (long) (60))))*1000); // 
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // Restore the interrupted status
+                    System.err.println("Metronome interrupted: " + e.getMessage());
+                }
+            }
+        }
+    }
+
+
     public void endMetronome() {
 
     }
