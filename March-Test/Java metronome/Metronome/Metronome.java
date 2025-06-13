3e223f559a26585f4c47cc48c9d9e97df3185014
Manish Reddy <ysmanish5@gmail.com>
2025-06-12 18:59:41 -0700

    Beginning to make metronome in java to see if I can get better timeliness of the beat with it

---
+++ March-Test/Java metronome/Metronome/Metronome.java
@@ -0,0 +1,11 @@
+import java.util.Timer;
+public class Metronome {
+    Timer timer = new Timer();
+    public void startMetronome() {
+        
+    }   
+    
+    public void endMetronome() {
+
+    }
+}
