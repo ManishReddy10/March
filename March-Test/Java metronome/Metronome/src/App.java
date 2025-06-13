427bd6d6e0a3f4320b47f2dab6bcc576f540b13e
Manish Reddy <ysmanish5@gmail.com>
2025-06-13 14:53:14 -0700

    added helper methods to java metronome as well as some more benchmarking

--- March-Test/Java metronome/Metronome/src/App.java
+++ March-Test/Java metronome/Metronome/src/App.java
@@ -1,8 +1,12 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
+        double bpm = 210;
+        System.out.println((60.0 / bpm) * 1000);
+        System.out.println((60.0 / bpm) * 1000);
+        
+        
         Metronome myMetronome = new Metronome();
-        myMetronome.startMetronome(60);
-
+        myMetronome.startMetronome(bpm);
     }
 }
--- March-Test/Java metronome/Metronome/src/Metronome.java
+++ March-Test/Java metronome/Metronome/src/Metronome.java
@@ -1,14 +1,14 @@
-import java.util.Timer;
 import java.time.Duration;
 import java.time.Instant;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-public class Metronome {
-    Timer timer = new Timer();
+
+public class Metronome {    
     
-    boolean isMetronomeOn = false;
+    private boolean isMetronomeOn = false;
+    private double bpm;
     
     public synchronized void playSound(final String url) {
     new Thread(new Runnable() {
@@ -36,8 +36,9 @@ public class Metronome {
     Instant start;
     Instant end;
     Duration elapsed;
-    public void startMetronome(double bpm) {
 
+    public void startMetronome(double bpm) {
+        this.bpm = bpm;
         
         isMetronomeOn = true;
         
@@ -47,7 +48,7 @@ public class Metronome {
             playMetronomeStrongBeat();
             synchronized (this) {
                 try {
-                    wait((1/((long) (bpm / (long) (60))))*1000); // 
+                    wait((long) ((60.0 / bpm) * 1000)); // 
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt(); // Restore the interrupted status
                     System.err.println("Metronome interrupted: " + e.getMessage());
@@ -55,13 +56,20 @@ public class Metronome {
             }
             end = Instant.now();
             elapsed = Duration.between(start, end);
-            System.out.println("Elapsed time in milliseconds: " + elapsed.toMillis());
+            System.out.println("Elapsed time in seconds: " + (elapsed.toMillis()/1000.0));
 
         }
     }
 
+    public void startMetronome() {
+        startMetronome(bpm);
+    }
+
+    public void setBpm(double bpm){
+        this.bpm = bpm;
+    }
 
     public void endMetronome() {
-
+        isMetronomeOn = false;
     }
 }
