e7b63a1801cbb72c656efafb3e523619f9bea008
Manish Reddy <ysmanish5@gmail.com>
2025-06-16 17:00:20 -0700

    trying and failing to run java code in python

--- March-Test/Java metronome/Metronome/src/App.java
+++ March-Test/Java metronome/Metronome/src/App.java
@@ -1,12 +1,22 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        double bpm = 210;
+        double bpm = 120;
         System.out.println((60.0 / bpm) * 1000);
         System.out.println((60.0 / bpm) * 1000);
         
         
         Metronome myMetronome = new Metronome();
         myMetronome.startMetronome(bpm);
+        // if (args.length < 1) {
+        //     System.out.println("Please provide BPM as an argument.");
+        //     return;
+        // }
+
+        // double bpm = Double.parseDouble(args[0]);
+        // Metronome myMetronome = new Metronome();
+        // myMetronome.startMetronome(bpm);
+        
+
     }
 }
