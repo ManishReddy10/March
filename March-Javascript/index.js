786104d06de1994c6b3811a2a2b9bbab7c218133
Manish Reddy <ysmanish5@gmail.com>
2025-06-16 22:56:05 -0700

    messing with the website

--- March-Javascript/index.js
+++ March-Javascript/index.js
@@ -1,4 +1,5 @@
 // console.log('Hello WOrld!');
 // window.alert('ALERT!');
 
-document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myH1").innerHTML = "<b> Hello </b>";
+document.getElementById("myH1").textContent = "Breathtaking website";
