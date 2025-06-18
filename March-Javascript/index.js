46a8012acebbaef2f64b3c2df99366098f9b6a7a
Manish Reddy <ysmanish5@gmail.com>
2025-06-18 16:13:58 -0700

    removing metronome code

--- March-Javascript/index.js
+++ March-Javascript/index.js
@@ -1,8 +1,3 @@
-// const { time } = require("console");
-
-
-// console.time("myTimer")
-// index.js
 const video = document.getElementById('video');
 const metronomeSound = new Audio('strong_beat.wav');
 const startMetronomeButton = document.getElementById('startMetronomeButton');
@@ -17,37 +12,3 @@ navigator.mediaDevices.getUserMedia({ video: true })
     alert('Could not access the camera. Please allow camera access or check your device.');
   });
 
-function startMetronome(bpm) {
-  setInterval(() => metronomeSound.play(), 500);
-  // console.timeStamp
-}
-
-startMetronome(120);
-
-function onStartMetronomeButtonClick() {
-  startMetronomeButton
-}
-
-class Metronome {
-
-  constructor(bpm) {
-
-  }
-
-  startMetronome() {
-
-  }
-
-  stopMetronome() {
-
-  }
-}
-
-// http://127.0.0.1:5500/March-Javascript/
-
-/**
- * Metronome logic
- * Start metronome
- * Play beat
- * 
- */
