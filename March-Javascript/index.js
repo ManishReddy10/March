692833317e82aa9aa5c064c890ca72ec1a6758fa
Manish Reddy <ysmanish5@gmail.com>
2025-06-17 20:05:32 -0700

    added buttons to start and stop metronome, code to play audio working, metronome code beginnings

--- March-Javascript/index.js
+++ March-Javascript/index.js
@@ -1,5 +1,7 @@
 // index.js
 const video = document.getElementById('video');
+const metronomeSound = new Audio('strong_beat.wav');
+const startMetronomeButton = document.getElementById('startMetronomeButton');
 
 navigator.mediaDevices.getUserMedia({ video: true })
   .then(stream => {
@@ -9,3 +11,22 @@ navigator.mediaDevices.getUserMedia({ video: true })
     console.error('Error accessing the camera:', error);
     alert('Could not access the camera. Please allow camera access or check your device.');
   });
+
+
+function startMetronome(bpm) {
+    setInterval(() => metronomeSound.play(),500);
+}
+
+
+startMetronome(120);
+
+function onStartMetronomeButtonClick() {
+    startMetronomeButton
+}
+
+/**
+ * Metronome logic
+ * Start metronome
+ * Play beat
+ * 
+ */
